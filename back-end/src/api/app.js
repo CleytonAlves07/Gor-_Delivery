350e90afef3f2ba88bcc834447d9b311bf3977ab
Almir-Araujo <araujo.almir@gmail.com>
2022-12-06 17:29:45 -0300

    Rota de Orders criada

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const errorMiddleware = require('../middleware/errorMiddleware');
 const routerLogin = require('../routes/routerLogin');
 const routerProducts = require('../routes/routerProducts');
+const routerOrders = require('../routes/routerOrders')
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.static('public'));
 
 app.use(routerLogin);
 app.use(routerProducts);
+app.use(routerOrders);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(errorMiddleware);
